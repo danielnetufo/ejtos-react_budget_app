4565278d5a1996ab7e0663071a06d9739aeac7aa
danielnetufo <daniel.netufo@gmail.com>
2024-02-14 11:01:35 +0100

    neew commit

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,25 @@
+import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseList = () => {
+    const [expenses]= useContext(AppContext);
+    return (
+        <table className='table'>
+            <thrend className="thread-light">
+                <tr>
+                    <th scope="col">Department</th>
+                    <th scope="col">Allocated Budget</th>
+                    <th scope="col">Incrrease</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thrend>
+            <tbody>
+                {expenses.map((expense) => (
+                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+export default ExpenseList;
